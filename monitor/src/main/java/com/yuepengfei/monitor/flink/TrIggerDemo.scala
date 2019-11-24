86dd96e6a1dac17c3c7a714dc877cffc8a3266da
day-ue <yuepngfei820@163.com>
2019-11-25 00:23:23 +0800

    添加flink状态管理，未完成

--- monitor/src/main/java/com/yuepengfei/monitor/flink/TrIggerDemo.scala
+++ monitor/src/main/java/com/yuepengfei/monitor/flink/TrIggerDemo.scala
@@ -1,14 +1,20 @@
 package com.yuepengfei.monitor.flink
 
-import java.util.Random
+import java.lang
+import java.util.{Map, Random}
 
+import org.apache.flink.api.common.state.{MapState, MapStateDescriptor, ValueState, ValueStateDescriptor}
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.functions.source.{RichParallelSourceFunction, SourceFunction}
+import org.apache.flink.streaming.api.functions.windowing.{ProcessWindowFunction, RichProcessWindowFunction}
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.windowing.triggers.{ProcessingTimeTrigger, Trigger, TriggerResult}
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+import scala.collection.JavaConverters._
 
 
 /**
@@ -32,6 +38,7 @@ object TriggerDemo {
         for (i <- 1 to 1000) {
           val random = new Random
           val message = "主流中的单词是"+words(random.nextInt(6))
+          println(message)
           ctx.collect(message)
           Thread.sleep(1000)
         }
@@ -42,11 +49,12 @@ object TriggerDemo {
       }
     })
 
-    dataStreamSource.map((_,1))
+    dataStreamSource.map((_,1)).setParallelism(1)
       .keyBy(_._1)
       .window(TumblingProcessingTimeWindows.of(Time.seconds(60)))
       .trigger(new MyTrigger)
-      .reduce((x,y)=>(x._1 , x._2 + y._2))
+      .process(new MyProcess())
+      //.reduce((x,y)=>(x._1 , x._2 + y._2)).setParallelism(1)
       .print("keyword")
 
     env.execute("triggerDemo")
@@ -54,22 +62,49 @@ object TriggerDemo {
   }
 }
 
+class MyProcess extends RichProcessWindowFunction[Int, Int, String, TimeWindow]{
+  /** process function维持的状态  */
+  lazy val state: MapState[String, Int] = getRuntimeContext
+    .getMapState(new MapStateDescriptor[String, Int]("myState",classOf[String], classOf[Int]))
+  override def process(key: String, context: ProcessWindowFunction[Int, Int, String, TimeWindow]#Context, elements: lang.Iterable[Int], out: Collector[Int]): Unit = {
+    elements.asScala.foreach(x =>{
+      if(state.contains(key)){
+        state.put(key, state.get(key) + x)
+      } else{
+        state.put(key, x)
+      }
+    })
+    state.entries.asScala.foreach(x => {
+      out.collect((x.getValue))
+    })
+
+  }
+}
+
+
 
 /**
  * 每来一条数据计算一次，一分钟清理一次状态
  */
 class MyTrigger extends Trigger[Any, TimeWindow]{
 
-  var beforeTime :Long= 0L
+  var count :Int= 0
 
   override def onElement(element: Any, timestamp: Long, window: TimeWindow, ctx: Trigger.TriggerContext): TriggerResult =
     {
-      ctx.registerProcessingTimeTimer(window.maxTimestamp)
-      return TriggerResult.FIRE
+      count = count + 1
+      if(count%10 == 0){
+        println("----------------------------每10个消息触发一次计算-------------------------------")
+        return TriggerResult.FIRE
+      }else{
+        ctx.registerProcessingTimeTimer(window.maxTimestamp)
+        return TriggerResult.CONTINUE
+      }
     }
 
   override def onProcessingTime(time: Long, window: TimeWindow, ctx: Trigger.TriggerContext): TriggerResult = {
-    TriggerResult.CONTINUE
+    println("--------------------------------一个滚动窗口触发一次计算-------------------------------------")
+    TriggerResult.FIRE_AND_PURGE
   }
 
   override def onEventTime(time: Long, window: TimeWindow, ctx: Trigger.TriggerContext): TriggerResult = return TriggerResult.CONTINUE
@@ -77,6 +112,7 @@ class MyTrigger extends Trigger[Any, TimeWindow]{
   @throws[Exception]
   override def clear(window: TimeWindow, ctx: Trigger.TriggerContext): Unit = {
     ctx.deleteProcessingTimeTimer(window.maxTimestamp)
+    println("--------------------------------------清理窗口数据----------------------------------------------------")
   }
 
   override def canMerge = true
