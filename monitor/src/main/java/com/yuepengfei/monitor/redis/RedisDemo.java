2961a148dba75816094bc7448721cbb9de978318
day-ue <yuepngfei820@163.com>
2019-12-19 23:28:50 +0800

    完成sparkStreaming自己管理offset

---
+++ monitor/src/main/java/com/yuepengfei/monitor/redis/RedisDemo.java
@@ -0,0 +1,13 @@
+package com.yuepengfei.monitor.redis;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisDemo {
+
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.240.131", 6379);
+        String offset = jedis.get("PrdKafkaWordCount.offsets");
+        System.out.println(offset);
+        jedis.close();
+    }
+}
--- monitor/src/main/java/com/yuepengfei/monitor/sparkstreaming/PrdKafkaWordCount.scala
+++ monitor/src/main/java/com/yuepengfei/monitor/sparkstreaming/PrdKafkaWordCount.scala
@@ -2,11 +2,13 @@ package com.yuepengfei.monitor.sparkstreaming
 
 import java.lang
 
+import org.apache.commons.lang3.StringUtils
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.kafka010.AssignStrategy
 import org.apache.spark.streaming.dstream.InputDStream
@@ -15,6 +17,7 @@ import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.{CanCommitOffsets, ConsumerStrategies, HasOffsetRanges, KafkaUtils, OffsetRange}
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import redis.clients.jedis.Jedis
+import scala.collection.JavaConverters._
 
 /**
  * 实际生产中kafka消费，维护offset
@@ -40,21 +43,26 @@ object PrdKafkaWordCount {
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
       "group.id" -> "use_a_separate_group_id_for_each_stream",
-      "auto.offset.reset" -> "latest",
+      "auto.offset.reset" -> "earliest",
       "enable.auto.commit" -> (false: lang.Boolean)
     )
-    val topics = Array("test")
-    val ds: InputDStream[ConsumerRecord[String, String]] = KafkaUtils.createDirectStream[String, String](scc, PreferConsistent, Subscribe[String, String](topics, kafkaParams))
 
-    new TopicPartition("","".toInt) -> "".toLong
-    KafkaUtils.createDirectStream[String, String](scc, PreferConsistent, ConsumerStrategies.Assign(null,kafkaParams,null))
+    var ds: InputDStream[ConsumerRecord[String, String]] = null
+
+    val ranges = readOffSet()
+    if (ranges == null){
+      val topics = Array("test")
+      ds = KafkaUtils.createDirectStream[String, String](scc, PreferConsistent, Subscribe[String, String](topics, kafkaParams))
+    } else {
+      val topicTartitions = ranges.map(_.topicPartition())
+      val offsets = ranges.map(x=>{x.topicPartition() -> x.untilOffset}).toMap
+      ds = KafkaUtils.createDirectStream[String, String](scc, PreferConsistent, ConsumerStrategies.Assign[String, String](topicTartitions ,kafkaParams,offsets))
+    }
+
     ds.foreachRDD {rdd =>{
       rdd.map(x=>x.value).toDF("word").createOrReplaceTempView("test")
       spark.sql("select word, count(1) from test group by word").show()
-      val ranges: Array[OffsetRange] = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
-      val client = new Jedis("192.168.240.131", 6379)
-      client.set("PrdKafkaWordCount.offsets","")
-      client.close()
+      saveOffSet(rdd)
       //这个kafka必须是1.0+
       //ds.asInstanceOf[CanCommitOffsets].commitAsync(ranges)
     }}
@@ -65,4 +73,27 @@ object PrdKafkaWordCount {
   }
 
 
+  def saveOffSet(rdd: RDD[_]): Unit ={
+    val ranges: Array[OffsetRange] = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+    val offsetRanges: Array[String] = ranges.map(x=>{s"${x.topic},${x.partition},${x.fromOffset},${x.untilOffset}"})
+    val str = StringUtils.join(offsetRanges.toList.asJava,"#@#")
+
+    val client = new Jedis("192.168.240.131", 6379)
+    client.set("PrdKafkaWordCount.offsets" ,str)
+    client.close()
+  }
+
+  def readOffSet(): Array[OffsetRange] = {
+    val client = new Jedis("192.168.240.131", 6379)
+    val offsets = client.get("PrdKafkaWordCount.offsets")
+    client.close()
+    if (offsets == null){
+      return null
+    }
+    val offsetRanges: Array[String] = offsets.split("#@#")
+    offsetRanges.map(x=>{
+      val offsetArr = x.split(",")
+      val topicPartition = new TopicPartition(offsetArr(0),offsetArr(1).toInt)
+      OffsetRange.create(topicPartition,offsetArr(2).toLong,offsetArr(3).toLong)})
+  }
 }
