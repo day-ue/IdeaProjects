f280766a6b8eced3e1bc47ef0cc86ea9500c5114
day-ue <yuepngfei820@163.com>
2019-12-25 22:08:59 +0800

    更新了paython代码

--- pythoncode/com/yuepengfei/spark/wordcount.py
+++ pythoncode/com/yuepengfei/spark/wordcount.py
@@ -1,7 +1,7 @@
 from pyspark import SparkContext
 if __name__ == "__main__":
     sc = SparkContext(master='local[*]', appName="pythonwordcount")
-    lines = sc.textFile('./words.txt')
+    lines = sc.textFile('file:/D:/code/day_ue/IdeaProjects/pythoncode/com/yuepengfei/spark/words.txt')
     result = lines.flatMap(lambda x: x.split(" ")).map(lambda x: (x, 1)).reduceByKey(lambda x, y: x+y).collect()
 
     for (word, count) in result:
