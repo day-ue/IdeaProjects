54743aafcd20c2e765479c6afe21b0f66b2dfdd1
day-ue <yuepngfei820@163.com>
2020-05-04 19:03:46 +0800

    flink任务转移到单独的项目中

--- monitor/src/main/java/com/yuepengfei/monitor/redis/RedisDemo.java
+++ monitor/src/main/java/com/yuepengfei/monitor/redis/RedisDemo.java
@@ -5,7 +5,7 @@ import redis.clients.jedis.Jedis;
 public class RedisDemo {
 
     public static void main(String[] args) {
-        Jedis jedis = new Jedis("192.168.240.131", 6379);
+        Jedis jedis = new Jedis("192.168.0.131", 6379);
         String offset = jedis.get("PrdKafkaWordCount.offsets");
         System.out.println(offset);
         jedis.close();
